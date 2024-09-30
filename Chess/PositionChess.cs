9b7eb5cf89c45289c5486eb572ac96f5944aa4c0
rodrigomsrocha <rodrigomtc82@gmail.com>
2024-09-30 16:24:47 -0300

    position in chess context

---
+++ Chess/PositionChess.cs
@@ -0,0 +1,26 @@
+using Board;
+
+namespace Chess
+{
+  class PositionChess
+  {
+    public char column { get; set; }
+    public int line { get; set; }
+
+    public PositionChess(char column, int line)
+    {
+      this.column = column;
+      this.line = line;
+    }
+
+    public Position toPosition()
+    {
+      return new Position(8 - line, column - 'a');
+    }
+
+    public override string ToString()
+    {
+      return "" + column + line;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -8,18 +8,9 @@ namespace Chess
   {
     public static void Main(string[] args)
     {
-      ChessBoard board = new ChessBoard(8, 8);
-      try
-      {
-        board.setPositionPiece(new Tower(board, Color.White), new Position(0, 0));
-        board.setPositionPiece(new Tower(board, Color.Black), new Position(1, 4));
-        board.setPositionPiece(new King(board, Color.Black), new Position(2, 4));
-        Screen.printBoard(board);
-      }
-      catch (BoardException e)
-      {
-        Console.WriteLine(e.Message);
-      }
+      PositionChess positionChess = new PositionChess('a', 1);
+      Console.WriteLine(positionChess);
+      Console.WriteLine(positionChess.toPosition());
     }
   }
 }
